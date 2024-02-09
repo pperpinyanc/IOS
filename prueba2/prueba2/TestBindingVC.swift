de0154d879543bfa4d8d8993234fb92d6a4f109e
Pablo Perpinan <pablo.perpinan@enti.cat>
2024-02-09 20:22:34 +0100

    primera clase

---
+++ prueba2/prueba2/TestBindingVC.swift
@@ -0,0 +1,27 @@
+//
+//  TestBindingVC.swift
+//  prueba2
+//
+//  Created by Pablo Perpinan on 9/2/24.
+//
+
+import UIKit
+
+class TestBindingVC: UIViewController {
+
+    
+    @IBOutlet weak var redBox: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+    @IBAction func helloWorld(_ sender: Any) {
+    }
+    
+
+}
---
+++ prueba2/prueba2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prueba2
+//
+//  Created by Pablo Perpinan on 9/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
